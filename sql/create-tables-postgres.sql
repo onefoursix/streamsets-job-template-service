16713d45e695dc662025e66a53add54281b2948b
Mark Brooks <mark@streamsets.com>
2023-11-30 18:35:21 -0800

    Update create-tables-postgres.sql

--- sql/create-tables-postgres.sql
+++ sql/create-tables-postgres.sql
@@ -3,13 +3,14 @@ create user streamsets with password 'streamsets';
 create schema streamsets authorization streamsets;
 
 create table streamsets.job_template(
-  job_template_id                     int               not null primary key,
-  delete_after_completion             boolean           not null,
+  job_template_id                     int                  not null primary key,
+  sch_job_template_id                 character varying    not null,
+  delete_after_completion             boolean              not null,
   source_runtime_parameters           jsonb,
   destination_runtime_parameters      jsonb,
   source_connection_info              jsonb,
   destination_connection_info         jsonb,
-  create_timestamp                    timestamp          not null
+  create_timestamp                    timestamp            not null
 );
 
 grant all on streamsets.job_template to streamsets;
@@ -27,17 +28,17 @@ grant all on streamsets.ingestion_pattern to streamsets;
 
 
 create table streamsets.ingestion_pattern_job_template_relationship(
-  relId                             int                  not null primary key,
+  rel_id                            int                  not null primary key,
   ingestion_pattern_id              int                  not null,
   job_template_id                   int                  not null,
   schedule                          character varying,
   CONSTRAINT ingestion_pattern_id
-      FOREIGN KEY(ingestion_pattern_id) 
+      FOREIGN KEY(ingestion_pattern_id)
     REFERENCES streamsets.ingestion_pattern(ingestion_pattern_id),
   CONSTRAINT job_template_id
-      FOREIGN KEY(job_template_id) 
+      FOREIGN KEY(job_template_id)
     REFERENCES streamsets.job_template(job_template_id)
-);  
+);
 
 grant all on streamsets.ingestion_pattern_job_template_relationship to streamsets;
 
@@ -62,5 +63,50 @@ create table streamsets.job_instance (
 );
 
 grant all on streamsets.job_instance to streamsets;
+
+
+
+insert into streamsets.ingestion_pattern (
+  ingestion_pattern_id,
+  pattern_name,
+  source,
+  destination,
+  create_timestamp
+)
+ values(1000, 'http-to-gcs', 'http','gcs', CURRENT_TIMESTAMP);
+
+insert into streamsets.job_template(
+
+  job_template_id,
+  sch_job_template_id,
+  delete_after_completion,
+  source_runtime_parameters,
+  destination_runtime_parameters,
+  source_connection_info,
+  destination_connection_info,
+  create_timestamp
+  ) values (
+    5000,
+    'c09f728a-2a73-4c7e-b735-2512039a9e6b:8030c2e9-1a39-11ec-a5fe-97c8d4369386',
+    false,
+    '{}',
+    '{}',
+    '{}',
+    '{}',
+    CURRENT_TIMESTAMP
+);
+
+
+insert into streamsets.ingestion_pattern_job_template_relationship (
+  rel_id,
+  ingestion_pattern_id,
+  job_template_id,
+  schedule
+) values (
+    100,
+    1000,
+    5000,
+   ''
+);
 
 
